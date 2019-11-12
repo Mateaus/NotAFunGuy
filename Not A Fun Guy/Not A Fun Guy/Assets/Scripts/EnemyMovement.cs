cbbdf0846505ac02c0a88c3f67553f1c90cca0eb
Ashley Garcia <asx133@my.utsa.edu>
2019-11-12 12:29:14 -0600

    Enemy Pathfinder

---
+++ Not A Fun Guy/Not A Fun Guy/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Transform target;
+    float speed = 2f;
+    float stop = 0.8f;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+    private void Update()
+    {
+        enemyDirection();
+       
+    }
+
+    public void enemyDirection()
+    {
+        Vector2 stayOnY = new Vector2(target.position.x, transform.position.y);
+
+        // Have enemies follow player only horizontally
+        if (Vector2.Distance(transform.position, target.position) > stop)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,stayOnY, speed * Time.deltaTime);
+        }
+
+
+    }
+
+
+}
