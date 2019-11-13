e5063322a4c996602c6a744f2d234668bf079627
Jaime <Jaimeperales123@gmail.com>
2019-11-13 11:10:40 -0600

    health script and enemy damage
    When the player comes in to contact with an object labeled "Enemy", 1 hp is taken from the player. As an example I labeled the box in the starting scene as an enemy

---
+++ Not A Fun Guy/Not A Fun Guy/Assets/DamageManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Enemy")) 
+        {
+            Debug.Log("Collision Made with enemy");
+           
+            PlayerHealth.damagePlayer();
+            Debug.Log("DamageTaken Health is now: " + PlayerHealth.getHealth());
+        }
+        
+    }
+}
