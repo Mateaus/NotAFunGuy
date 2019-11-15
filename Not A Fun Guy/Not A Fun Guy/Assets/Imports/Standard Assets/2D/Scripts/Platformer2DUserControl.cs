d13f97b0379d484f621cd77df51427e511fd09f1
Jaime <Jaimeperales123@gmail.com>
2019-11-15 09:36:10 -0600

    Smoothed stoping and fixed getting stuck at x = 50 bug
    the character controller has an xmin and xmax that can set the bounds for the character.

--- Not A Fun Guy/Not A Fun Guy/Assets/Imports/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Not A Fun Guy/Not A Fun Guy/Assets/Imports/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -13,6 +13,10 @@ namespace UnityStandardAssets._2D
         private bool m_Jump;
         public GameObject character;
 
+        //flags for checking if character is at levelBounds
+        bool atLeftBound = false;
+        bool atRightBound = false;
+
         //public int xmin,xmax;
 
 
@@ -54,11 +58,53 @@ namespace UnityStandardAssets._2D
 
                 m_Character.Move(h, crouch, m_Jump);
                 m_Jump = false;
-            if (m_Character.transform.position.x > xMax)
-                transform.position = new Vector3(xMax, transform.position.y, transform.position.z);
-            if (m_Character.transform.position.x < xMin)
-                transform.position = new Vector3(xMin, transform.position.y, transform.position.z);
 
+
+            if (m_Character.transform.position.x > xMax)
+            {
+                atRightBound = true;
+            }
+            else
+            {
+                atRightBound = false;
+            }
+
+
+            if (m_Character.transform.position.x < xMin)
+            {
+                //transform.position = new Vector3(xMin, transform.position.y, transform.position.z);
+                atLeftBound = true;
+                
+            }
+            else
+            {
+                atLeftBound = false;
+            }
+            if (atLeftBound)
+            {
+                if (h < 0 && m_Character.transform.position.x < xMin)
+                {
+                    character.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+                }
+                else
+                {
+                    character.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                    // character.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                }
+            }
+
+            if (atRightBound)
+            {
+                if (h > 0 && m_Character.transform.position.x > xMax)
+                {
+                    character.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+                }
+                else
+                {
+                    character.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                    // character.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                }
+            }
         }
     }
 }
