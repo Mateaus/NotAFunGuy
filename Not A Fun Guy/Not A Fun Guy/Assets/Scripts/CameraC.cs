3212b6c1eeecf414829da8a03cbc57df86b53c88
Mat <mateausc@gmail.com>
2019-11-17 15:49:23 -0600

    Added placeholder character to move around the level.

---
+++ Not A Fun Guy/Not A Fun Guy/Assets/Scripts/CameraC.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraC : MonoBehaviour
+{
+    public GameObject character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPosition = new Vector3(character.transform.position.x, character.transform.position.y, transform.position.z);
+        transform.position = currentPosition;
+    }
+}
