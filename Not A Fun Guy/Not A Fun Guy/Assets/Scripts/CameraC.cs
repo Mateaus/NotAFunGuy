f357836a9c72471a7c2a6f915159ab270946d480
Mat <mateausc@gmail.com>
2019-11-24 09:31:22 -0600

    Added Parallax to the first scene. Created a Hero prefab to then be used on the scene 2. Added Y-axis placeholder for camera.

--- Not A Fun Guy/Not A Fun Guy/Assets/Scripts/CameraC.cs
+++ Not A Fun Guy/Not A Fun Guy/Assets/Scripts/CameraC.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class CameraC : MonoBehaviour
 {
     public GameObject character;
-    // Start is called before the first frame update
+    public float cameraY;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        Vector3 currentPosition = new Vector3(character.transform.position.x, character.transform.position.y, transform.position.z);
+        Vector3 currentPosition = new Vector3(character.transform.position.x, character.transform.position.y + cameraY, transform.position.z);
         transform.position = currentPosition;
     }
 }
