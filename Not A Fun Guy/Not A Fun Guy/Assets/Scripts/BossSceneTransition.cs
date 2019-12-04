8909bc0a8bd4d9a24cd509acc01d00d30ef78031
Mateaus <mateausc@gmail.com>
2019-12-04 11:40:19 -0600

    Added scene transitions when walking out of bounds and moving platforms in the forest scene.

--- Not A Fun Guy/Not A Fun Guy/Assets/Scripts/BossSceneTransition.cs
+++ Not A Fun Guy/Not A Fun Guy/Assets/Scripts/BossSceneTransition.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class ForestSceneTransition : MonoBehaviour
+public class BossSceneTransition : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other) {
+   private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Forest");
+            SceneManager.LoadScene("BossRoom");
         }
     }
 }
---
+++ Not A Fun Guy/Not A Fun Guy/Assets/Scripts/PlatformRigidBodyHandler.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformRigidBodyHandler : MonoBehaviour
+{
+    public bool useTriggerAsSensor = false;
+    public List<Rigidbody2D> rigidbodies = new List<Rigidbody2D>();
+    public Vector3 lastPosition;
+    private Transform _transform;
+
+    private void Start()
+    {
+        _transform = transform;
+        lastPosition = _transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        if (rigidbodies.Count > 0)
+        {
+            for (int i = 0; i < rigidbodies.Count; i++)
+            {
+                Rigidbody2D rigidbody2D = rigidbodies[i];
+                Vector3 velocity = (_transform.position - lastPosition);
+                rigidbody2D.transform.Translate(velocity);
+            }
+        }
+
+        lastPosition = _transform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Rigidbody2D rigidbody2D = other.collider.GetComponent<Rigidbody2D>();
+        if (rigidbody2D != null)
+        {
+            AddRigidBody(rigidbody2D);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        Rigidbody2D rigidbody2D = other.collider.GetComponent<Rigidbody2D>();
+        if (rigidbody2D != null)
+        {
+            RemoveRigidBody(rigidbody2D);
+        }
+    }
+
+    private void AddRigidBody(Rigidbody2D rigidbody2D)
+    {
+        if (!rigidbodies.Contains(rigidbody2D))
+        {
+            rigidbodies.Add(rigidbody2D);
+        }
+    }
+
+    private void RemoveRigidBody(Rigidbody2D rigidbody2D)
+    {
+        if (rigidbodies.Contains(rigidbody2D))
+        {
+            rigidbodies.Remove(rigidbody2D);
+        }
+    }
+
+    //    private void OnCollisionEnter2D(Collision2D other) {
+    //        if (other.transform.tag == "Player")
+    //        {
+    //            other.transform.SetParent(transform);
+    //        }
+    //    }
+
+    //    private void OnCollisionExit2D(Collision2D other) {
+    //        if (other.transform.tag == "Player")
+    //        {
+    //            other.transform.SetParent(null);
+    //        }
+    //    }
+}
