3b204d838993f2b9e0685b6e1ea85b8222c56c8d
Mateaus <mateausc@gmail.com>
2019-12-06 02:21:05 -0600

    Added light sources to zone 2.

---
+++ Not A Fun Guy/Not A Fun Guy/Assets/Scripts/LightController/LightController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public Transform target;
+    public Light light;
+
+    private void Update()
+    {
+        if (target.position.y <= 0.0f)
+        {
+            transform.eulerAngles = new Vector3(Mathf.Clamp(Mathf.Abs(target.position.y * 11.0f), 0, 85.0f), 0, 0);
+        }
+
+        if (transform.eulerAngles.x > 60.0f)
+        {
+            if (light.range <= 25)
+            {
+                light.range += 7 * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (light.range <= 30 && light.range >= 0)
+            {
+                light.range -= 15 * Time.deltaTime;
+            }
+        }
+    }
+}
