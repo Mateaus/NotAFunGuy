c8adbe79272041faba218641234d9ba53878d445
Michael Graham <michael@thegrahamfam.com>
2019-12-05 20:54:53 -0600

    Enemies can now traverse angled terrain, but not efficiently

--- Not A Fun Guy/Not A Fun Guy/Assets/Scripts/EnemyMovement.cs
+++ Not A Fun Guy/Not A Fun Guy/Assets/Scripts/EnemyMovement.cs
@@ -6,11 +6,19 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     private Transform target;
+    private Rigidbody2D m_Rigidbody2D;
+    private Vector3 m_Velocity = Vector3.zero;
+
+
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
+
     Animator a;
     float speed = 2f;
     float stop = 0.8f;
+
     void Awake()
     {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
         a = GetComponent<Animator>();
     }
@@ -33,7 +41,21 @@ public class EnemyMovement : MonoBehaviour
         // Have enemies follow player only horizontally and only when player is at a certain distance
         if (distanceBetween > stop & distanceBetween <= 7.5)
         {
-            transform.position = Vector2.MoveTowards(transform.position, stayOnY , speed * Time.deltaTime);
+            float dir = 0;
+            if (transform.position.x < target.position.x)
+            {
+                dir = 1;
+            }
+            if (transform.position.x > target.position.x)
+            {
+                dir = -1;
+            }
+            //transform.position = Vector2.MoveTowards(transform.position, stayOnY , speed * Time.deltaTime);
+            // Move the character by finding the target velocity
+            Vector3 targetVelocity = new Vector2(dir * speed, m_Rigidbody2D.velocity.y);
+            // And then smoothing it out and applying it to the character
+            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
             move = true;
             attack = false;
         }
@@ -55,6 +77,4 @@ public class EnemyMovement : MonoBehaviour
 
 
     }
-
-
 }
