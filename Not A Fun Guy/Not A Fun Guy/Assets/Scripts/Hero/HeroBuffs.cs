e03dc4515fac71d0fb8a83f63f9d4fee742c9428
Michael Graham <michael@thegrahamfam.com>
2019-12-07 16:46:37 -0600

    Buffs can now be picked up, but do nothing to the character

---
+++ Not A Fun Guy/Not A Fun Guy/Assets/Scripts/Hero/HeroBuffs.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroBuffs : MonoBehaviour
+{
+    private static bool attackUp;
+    private static bool shield;
+    private static bool invuln;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("AttackUp"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("AttackUp");
+        }
+        else if (other.gameObject.tag.Equals("Shield"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Shield");
+        }
+        else if (other.gameObject.tag.Equals("Invuln"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Invuln");
+        }
+    }
+}
