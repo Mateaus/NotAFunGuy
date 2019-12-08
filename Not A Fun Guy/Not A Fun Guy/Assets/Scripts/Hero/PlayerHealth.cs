ce54eb43442800dd06c17d6164f218147f5a9989
Michael Graham <michael@thegrahamfam.com>
2019-12-08 16:08:59 -0600

    added particle effect for invulnerability

--- Not A Fun Guy/Not A Fun Guy/Assets/Scripts/Hero/PlayerHealth.cs
+++ Not A Fun Guy/Not A Fun Guy/Assets/Scripts/Hero/PlayerHealth.cs
@@ -6,7 +6,9 @@ public class PlayerHealth : MonoBehaviour
 {
     public int health;
     public static bool invuln = false;
+    private static bool active = false;
     public static bool shield = false;
+    public GameObject invEffect;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,18 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (invuln && !active)
+        {
+            active = true;
+            StartCoroutine("Inv");
+        }
+    }
+
+    IEnumerator Inv()
+    {
+        GameObject sparkle = Instantiate(invEffect, transform);
+        yield return new WaitForSeconds(10f);
+        Destroy(sparkle);
+        invuln = false;
     }
 }
